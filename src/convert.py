073b88013a4ef0aee330d484d9d34f7932177dce
roruther <Thereisafountain7!>
2020-11-09 12:54:07 -0500

    Fix: End def

--- src/convert.py
+++ src/convert.py
@@ -1,5 +1,8 @@
 #   Program to convert Celsius temperatures to Fahrenheit
+
 def main():
     celsius = eval(input("What is the Celsius temperature? "))
     fahrenheit = 9/5 * celsius + 32
     print("The temperature is", fahrenheit, "degrees Fahrenheit.")
+
+main()
